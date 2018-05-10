e7bc700b92753c0223c9bc04ceec660fef0808a3
B-Ballesteros <jballesteros.mtz@gmail.com>
2018-05-10 03:50:16 -0500

    Test Commit
    Added class in which the extension methods will be stored.
    Added function that transforms images directly to base64 strings.

---
+++ BasicOCR/MyExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace ExtensionMethods
+{
+    public static class MyExtensions
+    {
+        public static string ToBase64(this Image image)
+        {
+            string result;
+            using (var mStream = new MemoryStream())
+            {
+                image.Save(mStream, ImageFormat.Png);
+                var binaryImage = mStream.ToArray();
+                result = Convert.ToBase64String(binaryImage);
+            }
+            return result;
+        }
+    }
+}
